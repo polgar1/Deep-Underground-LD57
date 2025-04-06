af311ef47ef027e7c13dd6965e4116c6ca94443b
polgar1 <punga9111@gmail.com>
2025-04-06 16:58:32 +0300

    Adjust flashlight and color map

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,11 @@ extends CharacterBody3D
 
 @export var speed = 15
 @onready var anim : AnimationPlayer = $Player/AnimationPlayer
+@onready var movement_audio : AudioStreamPlayer3D = $AudioStreamPlayer3D
+
+func _ready() -> void:
+	#Shader is disabled in the editor because it messes up things
+	$Camera3D/Shader.visible = true
 
 func _process(delta: float) -> void:
 	var direction : Vector2 = Vector2(0, 0)
@@ -15,8 +20,11 @@ func _process(delta: float) -> void:
 	velocity.y = direction.y * speed * delta
 	
 	if velocity.y:
+		if not movement_audio.playing:
+			movement_audio.play()
 		anim.play("ArmatureAction")
 	else:
+		movement_audio.stop()
 		anim.pause()
 	
 	move_and_slide()
