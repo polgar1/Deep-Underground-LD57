8121e45807ddb6162d08e3858d6521443aa85792
polgar1 <punga9111@gmail.com>
2025-04-07 12:24:45 +0300

    Add mouse cursor

--- scripts/interaction_raycast.gd
+++ scripts/interaction_raycast.gd
@@ -1,8 +1,14 @@
 extends RayCast3D
 
+@onready var cursor : ColorRect = get_tree().get_first_node_in_group("cursor")
+
 func _process(delta: float) -> void:
 	if is_colliding():
 		var collider : StaticBody3D = get_collider()
-		if collider and collider.collision_layer == 2 and Input.is_action_just_released("Interact"):
-			if collider.has_method("Interact"):
-				collider.Interact()
+		if collider and collider.collision_layer == 2:
+			cursor.color = Color.RED
+			if Input.is_action_just_released("Interact"):
+				if collider.has_method("Interact"):
+					collider.Interact()
+	else:
+		cursor.color = Color.WHITE
